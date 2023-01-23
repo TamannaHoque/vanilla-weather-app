861c0f4137101a65917b0e11fe9747007591eca4
Tamanna Hoque <tamanna98@hotmail.co.uk>
2023-01-23 22:12:46 +0000

    Added API to Javascript

--- src/app.js
+++ src/app.js
@@ -16,12 +16,14 @@ let minutes = now.getMinutes();
 minutes = minutes <= 9 ? "0" + minutes : minutes;
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperature = document.querySelector("#temperature");
   let city = document.querySelector("#city");
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
   let date = document.querySelector("#date");
+  let icon = document.querySelector("#icon");
 
   temperature.innerHTML = Math.round(response.data.temperature.current);
   city.innerHTML = response.data.city;
@@ -29,9 +31,10 @@ function displayTemperature(response) {
   humidity.innerHTML = response.data.temperature.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
   date.innerHTML = `${day} ${hour}:${minutes}`;
+  icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.condition.icon_url[0]}@2x.png`);
 }
 
 let apiKey = "86bfa0b913987f39af8002aa2oe74a1t";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=86bfa0b913987f39af8002aa2oe74a1t&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
